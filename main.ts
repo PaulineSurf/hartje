5729b709fc245e3c33ab96cafa9e35b97adff48d
PaulineSurf <pauline.de.cuyper11@gmail.com>
2025-02-06 14:04:24 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . # # # .
-        . . # . .
-        . . . . .
-        `)
+    basic.showString("Pauline")
 })
